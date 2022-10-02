5d85d4a8488baefbf3a84a8ec403e576532dd409
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-02 18:33:44 +0100

    Ex3 feito

--- Pratica1/Ex3.py
+++ Pratica1/Ex3.py
@@ -1,21 +1,24 @@
-maximum_number = 9999
+ #!/usr/bin/env python
+
+from colorama import Fore, Back, Style  
 
 
+maximum_number = 11
 def isPrime(x):
     
     # <Fill the blank>
-    # O valor que vem como argumento da função is prime vai ser o limite superior
-    # Um numero é primo quando é divisivel por si e por 1.
+    
     for i in range(2, x):
-         # Nesse sentido no inicio verifica-se o numero é divisivel por 1
-         # De seguida verifica-se caso ele é divisivel por si mesmo
-         resultado = x % i
+        
+         resultado = x % i         
          if resultado == 0:
-             # Se o modulo da divisao for igual a zero ele:
+            # Faz o print do divisor dos numeros nao primos, aparece acima
+            # Para que na lista que estamos a dar print asseguir apareça o o numero 2 é necessario que a lista contenha mais  
+            #do que o próprio elemento, nesse sentido se o unico divisor por causa do qual o 4 não é numero primo 
+            # é o divisor 2 nesse sentido para ele aparecer na lista que fazemos print ele tem chegar a esse elemento e devido a isso nós adicionamos o + 1 
+             print('O divisor para qual o numero ' + str(x) +  ' não é primo é' + str(list(range(2,i + 1))))           
              return False
-             
-            
-         
+
     return True
 
 
@@ -23,24 +26,17 @@ def isPrime(x):
 
 def main():
         print("Starting to compute prime numbers up to " + str(maximum_number))
-        # Vamos Percorrer um ciclo for de 0 ate ao numero maximo nao inclusive
+        
         for i in range(1, maximum_number+1):
-            # ao entrar dentro do ciclo for ele chama a função isPrime e leva como argumento o numero correspondente ao i
+     
             if isPrime(i):
-                # Caso função isPrime retornar verdadeiro entao faz-se o seguinte printe
-                print('Number ' + str(i) + ' is prime.')
+                # Ao definir o Style afrente estamos a definir 
+                print(Style.BRIGHT +  Fore.BLUE +   'Number ' + str(i) + ' is prime.' + Style.RESET_ALL)
 
             else:
-                # Caso a função isPrime retornar falso entao faz-se o seguinte
-                print('Number ' + str(i) + ' is not prime.')
+               
+                print(Fore.RED +    'Number ' + str(i) + ' is not prime.'  + Style.RESET_ALL)
 
-# Para fazer o exercicio 2.2 de numero menor a 10000
-# É preciso na janela de comando meter: 
-# python3 Ex2.py | grep "is prime" | grep "3" |  wc -l
-# Cada um dos comandos acima são sequenciais ou seja vai primeiro reproduzir o programa
-# De seguida vai ver os primos 
-# De seguida vai ver aqueles que tem os 3  
-# E no final....
 
 
 if __name__ == "__main__":
