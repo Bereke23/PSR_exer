7cc7cbacf180be004c70af5de58ab602f4156c19
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-04 01:10:43 +0100

    inicio ex 4

---
+++ Pratica2/Ex4/main.py
@@ -0,0 +1,23 @@
+import readchar
+
+
+def printAllCharsUpTo(stop_char):
+    print('1')
+    # <to complete>
+
+
+
+
+
+def main(): 
+    key = readchar.readkey()
+    # <to complete>
+
+
+
+
+
+
+
+if __name__ == '__main__':
+        main()
