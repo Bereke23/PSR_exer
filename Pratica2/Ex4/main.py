80af79ce332af11b63cb387d5ef3c6165342618b
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-06 00:43:07 +0100

    Exercicio 4.a)

--- Pratica2/Ex4/main.py
+++ Pratica2/Ex4/main.py
@@ -1,8 +1,16 @@
+from email.headerregistry import ContentTransferEncodingHeader
+from queue import Empty
 import readchar
 
 
 def printAllCharsUpTo(stop_char):
-    print('1')
+
+    list = []
+    print(stop_char)
+    for i in range(34,stop_char+1):
+        list.append(chr(i))
+    print(list)
+
     # <to complete>
 
 
@@ -11,8 +19,11 @@ def printAllCharsUpTo(stop_char):
 
 def main(): 
     key = readchar.readkey()
+    
+    if key is not Empty:
+        print('o caracter final ' + key)
+        printAllCharsUpTo(ord(key)) 
     # <to complete>
-
 
 
 
