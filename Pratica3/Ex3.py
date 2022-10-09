4ed0b82e40e267f9a658f03d2cbb3ae22e5ca57d
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-09 01:38:20 +0100

    Ex3 incompleto

--- Pratica3/Ex3.py
+++ Pratica3/Ex3.py
@@ -4,15 +4,19 @@ Complex = namedtuple('Complex', ['r', 'i'])
 
 
 def addComplex(x, y):
+    print('1')
     # addapt code to use named tuples
 
 def multiplyComplex(x, y):
     # addapt code to use named tuples
+    print('1')
 
 def printComplex(x):
+    print('1')
     # addapt code to use named tuples
 
 def main():
+    
     # define two complex numbers as tuples of size two
     c1 = Complex(5, 3)  # use order when not naming
     c2 = Complex(i=7, r=-2)  # if items are names order is not relevant
@@ -20,7 +24,6 @@ def main():
 
     # Test add
     addComplex(c1, c2)
-    printComplex(c3)
 
     # test multiply
     printComplex(multiplyComplex(c1, c2))
