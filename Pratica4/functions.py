02fdf9a843d0ac511c3281554ad75dedb60295e8
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-09 01:38:46 +0100

    As funçoes usadas

---
+++ Pratica4/functions.py
@@ -0,0 +1,83 @@
+import argparse
+from operator import truediv
+import readchar
+from queue import Empty
+import time
+import math 
+from colorama import Fore, Back, Style
+import random
+Tempo_maximo = 10
+init = time.time()
+fin = 0
+
+def argumentosetrada():
+    # Definição dos argumentos de entrada:
+    parser = argparse.ArgumentParser(description='Modo de funcionamento')
+    parser.add_argument('-utm','--use_time_mode', action='store_true' ,
+                    help='Max number of secs for time mode or maximum number os inputs mode.')
+    parser.add_argument('-mv','--max_value',type = int, required= True,
+                    help='Max number of secs for time mode or maximum number os inputs mode.')
+    args = vars(parser.parse_args())
+    valor_max = args['max_value']
+    temporizador = args['use_time_mode']
+    print(args)
+    teclainicial(valor_max,temporizador)
+    
+
+def teclainicial(numero_maximo, temporizador):
+    # Definição da parte inicial do teste:
+    seconds = time.time()
+    local_time = time.ctime(seconds)
+    print(Fore.BLUE + "PARI" + Style.RESET_ALL + " Typing Test, Grupo 6," + local_time + Style.RESET_ALL ) 
+    if temporizador:
+        print("Test runnig up to " + str(Tempo_maximo) + "s")
+    else:
+        print("Test runnig up to " + str(numero_maximo) + " inputs" )
+    print("Press any key to star the test")
+    key = readchar.readkey()
+    if key is not Empty:
+        modofuncionamento(numero_maximo,temporizador)
+    
+
+def modofuncionamento(numero_maximo,temporizador):
+    # Escolha do tipo do funcionamento:
+    if temporizador:
+        inicio = time.time()
+        intervalo = 0
+        while intervalo < Tempo_maximo:
+            randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+            print("Type letter " + randomLowerLetter)
+            tecla = readchar.readkey()
+
+            if randomLowerLetter == tecla:
+                print('The key pressed ' + Fore.GREEN + tecla + Style.RESET_ALL)
+            else:
+                print('The key pressed ' + Fore.RED+ tecla + Style.RESET_ALL)
+            leitura = readchar.readkey()
+            if leitura == '1':
+                exit(0)
+            fim = time.time()
+            intervalo = fim - inicio
+        print( "Current test duration " + "(" + str(intervalo)+ ")" + " exceeds maximum of 10")
+        Terminiodeseccao()
+    else:
+        for letras in range(1,numero_maximo+1):
+            randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+            print("Type letter " + randomLowerLetter)
+            tecla = readchar.readkey()
+        
+            if randomLowerLetter == tecla:
+                print('The key pressed ' + Fore.GREEN + str(tecla) + Style.RESET_ALL)
+            else:
+                print('The key pressed ' + Fore.RED+ str(tecla) + Style.RESET_ALL)
+            leitura = readchar.readkey()
+            if leitura == '1' :
+                exit(0)
+        Terminiodeseccao()
+
+
+def Terminiodeseccao():
+    fin = time.time()
+    Duraçao = fin - init
+    #print( "Current test duration " + str(Duraçao)+ "s")
+    print(Fore.BLUE + "Test finished!!" + Style.RESET_ALL)
