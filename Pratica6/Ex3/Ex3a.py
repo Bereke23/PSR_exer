32d2361ffa068317a11bb7d3248db40e86e7f6da
Bereke23 <berekeakmetzhanov4@gmail.com>
2022-10-26 19:32:23 +0100

    Aleraçao espaço

--- Pratica6/Ex3/Ex3a.py
+++ Pratica6/Ex3/Ex3a.py
@@ -1,26 +1,14 @@
-from ast import While
+#!/usr/bin/env python3
+from copy import deepcopy
 import cv2
 # Abre a imagem
+gui_image = None
 def main():
-    #-------------
-    # Initialization 
-    #-------------
-
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    # Read the input image
     video = cv2.VideoCapture(0)
-    #-------------
-    # Execution 
-    #-------------
     if not video.isOpened():
         print("Cannot open camera")
         exit()
-    #-------------
-    # Processing 
-    #-------------
-    #-------------
-    # Visualization 
-    #-------------
 
     while True:
         _, img = video.read()
@@ -34,8 +22,10 @@ def main():
         )
         for (x, y, w, h) in faces:
             verde = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2).copy()
+            gui_image = deepcopy(verde)
+            
         for (x, y, w, h) in faces:
-            dst = cv2.addWeighted(img, 0.1, verde, 0.5, 0.0)
+            dst = cv2.addWeighted(img, 0.1, gui_image, 0.5, 0.0)
         
         cv2.imshow('Deteção da cara', dst)
 
@@ -44,11 +34,7 @@ def main():
     video.release()
     cv2.destroyAllWindows()
         
-        
-        
-    #-------------
-    # Termination 
-    #-------------
+    
 
 
 if __name__ == '__main__':
